89b7b48905fcdca628f06d8f7358282c825aa2c8
Rudbeckia.hirta.L <83773602@qq.com>
2018-08-03 12:09:37 +0800

    VPC

--- Utils/SeeEvent.py
+++ Utils/SeeEvent.py
@@ -10,12 +10,6 @@ from __future__ import print_function
 import datetime
 import boto3
 
-
-# print('Loading function')
-
-# ec2 = boto3.resource("ec2")
-# iam = boto3.resource("iam")
-
 def lambda_handler(event, context):
     import json
     tran(json.dumps(event, indent=2))
@@ -34,11 +28,5 @@ def tran(str_test):
 
 
 if __name__ == '__main__':
-    # event = {
-    # "awslogs": {
-    # "data": "H4sIAAAAAAAAAHWPwQqCQBCGX0Xm7EFtK+smZBEUgXoLCdMhFtKV3akI8d0bLYmibvPPN3wz00CJxmQnTO41whwWQRIctmEcB6sQbFC3CjW3XW8kxpOpP+OC22d1Wml1qZkQGtoMsScxaczKN3plG8zlaHIta5KqWsozoTYw3/djzwhpLwivWFGHGpAFe7DL68JlBUk+l7KSN7tCOEJ4M3/qOI49vMHj+zCKdlFqLaU2ZHV2a4Ct/an0/ivdX8oYc1UVX860fQDQiMdxRQEAAA=="
-    # }
-    # }
-    # lambda_handler(event,"")
 
     tran()
