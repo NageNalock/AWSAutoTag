60567e39e294c69a72fb8554dbf02f6c64b81de1
Rudbeckia.hirta.L <83773602@qq.com>
2018-08-04 11:29:57 +0800

    bug

--- AWSAutotagTotal/AWSAutoTagTotalV3.py
+++ AWSAutotagTotal/AWSAutoTagTotalV3.py
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
     detail = event['detail']
     eventname = detail['eventName']
 
-    tagger = awstaglib.Tagger(detail)
+    tagger = awstaglib.Tagger(event)
 
     if eventname == 'RunInstances':
         # 启动 EC2
